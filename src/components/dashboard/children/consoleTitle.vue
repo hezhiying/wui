756fab511f78a353840528c7e6344579dc651395
H <21418592@qq.com>
2017-04-13 15:15:36 +0800

    优化title

--- src/components/dashboard/children/consoleTitle.vue
+++ src/components/dashboard/children/consoleTitle.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="console-title console-title-border margin-bottom-24">
         <div class="pull-left">
-            <h3>
-                <slot> </slot>
-            </h3>
+              <template v-if="haveHTag"><slot></slot></template>
+              <template v-else><h3><slot></slot></h3></template>
         </div>
         <div class="pull-right">
             <slot name="right"> </slot>
@@ -12,11 +11,22 @@
 </template>
 
 <script lang="babel">
+import {oneOf} from '../../../utils/fn.js'
     export default{
         name:'ConsoleTitle',
         data(){
             return{
-                msg:'hello vue'
+                haveHTag:false
+            }
+        },
+        created(){
+            //如果未包含H元素，默认添加h3
+            if(this.$slots.default){
+                this.$slots.default.map(item=>{
+                    if(oneOf(item.tag,['h1','h2','h3','h4','h5'])){
+                        this.haveHTag = true;
+                    }
+                })
             }
         },
         components:{
