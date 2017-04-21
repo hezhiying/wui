b17282b2352be51ee9e002116de0b15af63dec19
H <21418592@qq.com>
2017-04-21 10:30:15 +0800

    添加this.$Ajax使用方法

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import iView from './iview'
 import components from './components'
 import dashboard from './pages/dashboard'
 import Dialog from './components/dialog'
-
+import ajax from './utils/ajax'
 Vue.use(iView);
 Vue.use(components);
 window.Vue = Vue;
 window.iView = iView;
+Vue.prototype.$Ajax = ajax;
 
 window.Dialog = Dialog;
 window.jQuery = window.$ = jQuery;
